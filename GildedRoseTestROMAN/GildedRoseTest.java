0e073388db65068d828cb04658dcccf8d93a42e7
Roman Mysyura <117473844+RomanMysyura@users.noreply.github.com>
2024-05-03 10:35:19 +0200

    Romantest2 (#47)
    * historias added
    
    * Update
    
    * Update Roman
    
    * PDF Duplicado eliminado
    
    * JUNIT Test added
    
    * JUNIT Test added

--- GildedRoseTestROMAN/GildedRoseTest.java
+++ GildedRoseTestROMAN/GildedRoseTest.java
@@ -58,4 +58,18 @@ class GildedRoseTest {
     }
     
     
+
+    @Test @DisplayName(value= "Sulfuras Test")
+    void sulfurasQuality() {
+        
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 11, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", items[0].name, "Name should remain unchanged");
+        assertEquals(11, items[0].sellIn, "SellIn should remain unchanged");
+        assertEquals(80, items[0].quality, "Quality should remain unchanged");
+    }
+    
+    
+    
 }
